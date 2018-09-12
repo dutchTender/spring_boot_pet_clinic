fbe1236f26d5341e31676ac8201cb9bd2956a9d6
dutchTender <lzhang421@gmail.com>
2018-09-12 17:25:45 -0400

    spring intializer

---
+++ src/main/java/com/metatron/tech/spring_boot_pet_clinic/SpringBootPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.metatron.tech.spring_boot_pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPetClinicApplication.class, args);
+    }
+}
