f653a5f39b2949ce2c4860982fe9aef20f4ebf45
dutchTender <lzhang421@gmail.com>
2018-09-18 13:03:46 -0400

    added map implementation for the POJO models closes #3.

---
+++ spring_boot_pet_clinic_data/src/main/java/com/metatron/tech/model/map/PetServiceMap.java
@@ -0,0 +1,37 @@
+package com.metatron.tech.model.map;
+
+import com.metatron.tech.model.Pet;
+import com.metatron.tech.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+      return   super.save(object.getId(),object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+}
---
+++ spring_boot_pet_clinic_data/src/main/java/com/metatron/tech/model/map/VetServiceMap.java
@@ -0,0 +1,36 @@
+package com.metatron.tech.model.map;
+
+import com.metatron.tech.model.Vet;
+import com.metatron.tech.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+        super.deleteById(id);
+
+    }
+}
