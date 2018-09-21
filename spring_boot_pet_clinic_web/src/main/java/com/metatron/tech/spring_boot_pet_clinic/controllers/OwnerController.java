ea93bf1375e7bb28917ca4fbcb706a478cde8060
dutchTender <lzhang421@gmail.com>
2018-09-20 23:42:28 -0400

    updatred owners contorller and implmented owners page that lists all of the owners. closes #14.

--- spring_boot_pet_clinic_web/src/main/java/com/metatron/tech/spring_boot_pet_clinic/controllers/OwnerController.java
+++ spring_boot_pet_clinic_web/src/main/java/com/metatron/tech/spring_boot_pet_clinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.metatron.tech.spring_boot_pet_clinic.controllers;
 
+import com.metatron.tech.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/","/owners", "/owners/index","/owners/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
         // get owner info
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
